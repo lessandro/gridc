276e96bedfa7266727452efb37dfe52dd919eb66
Lessandro Mariano <lessandro@gmail.com>
2012-12-30 21:35:17 -0200

    Strip comments before parsing

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,20 @@ import System.Environment
 import GridC.Parser
 import GridC.Codegen
 
+uncomment :: String -> String
+uncomment (x:y:xs)
+    | x == y && y == '/' = ""
+    | otherwise = x : uncomment (y : xs)
+uncomment xs = xs
+
+preprocess :: String -> String
+preprocess = unlines . map uncomment . lines
+
 main :: IO ()
 main = do
     args <- getArgs
     contents <- readFile $ head args
-    let ast = parseGC contents
+    let processed = preprocess contents
+    let ast = parseGC processed
     let output = codegen ast
     putStr $ unlines output
